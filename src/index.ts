97de2a077f7417ae859b8dca36898e0f3d52cc9e
kazuhe <omc.ohara@gmail.com>
2022-03-27 23:37:24 +0900

    Null check

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const LIST = ["foo", "bar", "Baz", "qux", "piyo piyo"];
   const container = document.querySelector("#container");
-  for (let i = 0; i < LIST.length; i++) {
-    const p = document.createElement("p");
-    const text = LIST[i];
-    const words = text.match(/[a-z]+/gi);
-    if (!words) return;
-    const result = words
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      .join("");
-    p.textContent = result;
-    // @ts-expect-error
-    container.appendChild(p);
+  if (container !== null) {
+    for (let i = 0; i < LIST.length; i++) {
+      const p = document.createElement("p");
+      const text = LIST[i];
+      const words = text.match(/[a-z]+/gi);
+      if (!words) return;
+      const result = words
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join("");
+      p.textContent = result;
+      container.appendChild(p);
+    }
   }
 });
